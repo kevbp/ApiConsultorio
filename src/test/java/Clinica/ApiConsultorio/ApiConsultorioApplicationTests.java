fa742ffb902db62a15b243eeb118f21879c7cfc9
Kevin Broncano <kevinbroncanop@gmail.com>
2025-12-05 17:49:17 -0500

    Update

--- src/test/java/Clinica/ApiConsultorio/ApiConsultorioApplicationTests.java
+++ src/test/java/Clinica/ApiConsultorio/ApiConsultorioApplicationTests.java
@@ -1,10 +1,10 @@
-package Clinica.Consultorio;
+package Clinica.ApiConsultorio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConsultorioApplicationTests {
+class ApiConsultorioApplicationTests {
 
 	@Test
 	void contextLoads() {
